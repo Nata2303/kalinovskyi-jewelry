da7d346db9ed974a889094403643813b4400f58b
Nata2303 <121019039+Nata2303@users.noreply.github.com>
2023-06-08 15:56:50 +0000

    Deploying to gh-pages from @ Nata2303/kalinovskyi-jewelry@fbc17da81518607332818b69a4d6cf37c07382cf 🚀

---
+++ index.78373024.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector("[data-menu-open]"),n=document.querySelector("[data-menu-close]"),t=document.querySelector(".mobile-menu");e.addEventListener("click",(function(){t.classList.add("is-open")})),n.addEventListener("click",(function(){t.classList.remove("is-open")}))}));
+//# sourceMappingURL=index.78373024.js.map
---
+++ index.babdbcca.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){const e=document.querySelector("[data-menu-open]"),n=document.querySelector("[data-menu-close]"),t=document.querySelector(".mobile-menu");e.addEventListener("click",(function(){t.classList.add("is-open")})),n.addEventListener("click",(function(){t.classList.remove("is-open")}))}));
+//# sourceMappingURL=index.babdbcca.js.map
