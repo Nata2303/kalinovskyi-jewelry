718fe45dfce1c121d35f32ac742eb1e0bbea0283
Nata2303 <121019039+Nata2303@users.noreply.github.com>
2023-06-14 11:43:48 +0000

    Deploying to gh-pages from @ Nata2303/kalinovskyi-jewelry@ed836d0fe22260534e1ff6b19001bd952db18e62 🚀

---
+++ index.1642709a.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),openSecondModalBtn:document.querySelector("[data-modal-open-second]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openSecondModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.1642709a.js.map
---
+++ index.c8e13b24.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),openSecondModalBtn:document.querySelector("[data-modal-open-second]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openSecondModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.c8e13b24.js.map
