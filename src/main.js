83121e6039b30e696366c6b0fa7595c7367aa251
Nata2303 <121019039+Nata2303@users.noreply.github.com>
2023-06-30 23:22:22 +0400

    modal

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
   const mobileMenuButton = document.querySelector('[data-menu-open]');
   const mobileMenuCloseButton = document.querySelector('[data-menu-close]');
+  const contactManagerButton = document.getElementById('contactManager-link');
   const mobileMenu = document.querySelector('.mobile-menu');
+  const modal = document.querySelector('[data-modal]');
 
   mobileMenuButton.addEventListener('click', function () {
     mobileMenu.classList.add('is-open');
   });
 
   mobileMenuCloseButton.addEventListener('click', function () {
+    mobileMenu.classList.remove('is-open');
+  });
+
+  contactManagerButton.addEventListener('click', function () {
     mobileMenu.classList.remove('is-open');
   });
 });
