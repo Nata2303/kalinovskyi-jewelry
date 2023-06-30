4d6631885e8bbaddc084b1c13732eb23864bae88
Nata2303 <121019039+Nata2303@users.noreply.github.com>
2023-06-30 23:06:32 +0400

    modal

--- src/modal.js
+++ src/modal.js
@@ -1,14 +1,24 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    openSecondModalBtn: document.querySelector('[data-modal-open-second]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.openSecondModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+document.addEventListener('DOMContentLoaded', function () {
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      openSecondModalBtn: document.querySelector('[data-modal-open-second]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+      backdrop: document.querySelector('.backdrop'),
+    };
+    refs.openModalBtn.addEventListener('click', openModal);
+    refs.openSecondModalBtn.addEventListener('click', openModal);
+    refs.closeModalBtn.addEventListener('click', closeModal);
+
+    function openModal() {
+      refs.modal.classList.add('is-visible');
+      refs.backdrop.classList.add('is-visible');
+    }
+
+    function closeModal() {
+      refs.modal.classList.remove('is-visible');
+      refs.backdrop.classList.remove('is-visible');
+    }
+  })();
+});
