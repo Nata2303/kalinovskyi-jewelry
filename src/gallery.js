22dda9ff0102b25854a2c64cea0861d62cd0f317
Nata2303 <121019039+Nata2303@users.noreply.github.com>
2023-07-08 18:04:16 +0400

    swiper

--- src/gallery.js
+++ src/gallery.js
@@ -1,7 +1,25 @@
-  // Ініціалізація Swiper
-  const swiper = new Swiper('.swiper', {
+$(document).ready(function () {
+  const swiper = new Swiper('.gallery', {
     slidesPerView: 1,
     spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 650px
+      650: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      // when window width is >= 1040px
+      1040: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
     loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
@@ -19,4 +37,4 @@
 
   // Оновлення Swiper після додавання слайдів
   swiper.update();
-
+});
