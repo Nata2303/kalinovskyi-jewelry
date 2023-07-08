6e96701caf448b693d0718c1b36423f69b45e664
Nata2303 <121019039+Nata2303@users.noreply.github.com>
2023-07-08 14:05:20 +0000

    Deploying to gh-pages from @ Nata2303/kalinovskyi-jewelry@22dda9ff0102b25854a2c64cea0861d62cd0f317 🚀

--- gallery.394815d3.js
+++
@@ -1,2 +0,0 @@
-const e=new Swiper(".swiper",{slidesPerView:1,spaceBetween:10,loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});$(".gallery-img").each((function(e){$(this).addClass("swiper-slide"),0===e&&$(this).addClass("swiper-slide-active")})),e.update();
-//# sourceMappingURL=gallery.394815d3.js.map
--- gallery.d0046312.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=new Swiper(".swiper",{slidesPerView:1,spaceBetween:10,loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});$(".gallery-img").each((function(e){$(this).addClass("swiper-slide"),0===e&&$(this).addClass("swiper-slide-active")})),e.update()}();
-//# sourceMappingURL=gallery.d0046312.js.map
